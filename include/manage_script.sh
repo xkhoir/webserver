d6ddea8e5286c6aa4b14f429d77c8534b3412b9b
xkhoir <robby.khoirun1@gmail.com>
2023-04-05 22:03:43 +0700

    set sleep to 2

--- include/manage_script.sh
+++ include/manage_script.sh
@@ -5,9 +5,11 @@
 cek_new () {
     # Check if there are updates available
     if [[ $(apt list --upgradable 2>/dev/null | wc -l) -gt 1 ]]; then
+        clear
         echo -e "\nUpdates are available. Running apt update and upgrade."
         manage_server update
         echo -e "\nUpdates Success !!."
+        clear
     fi
 }
 
@@ -20,8 +22,11 @@ cek_con() {
         # Jika menerima balasan dari google.com, koneksi internet tersedia
         return 0
     else
+        clear
         # Jika tidak menerima balasan dari google.com, koneksi internet tidak tersedia
         echo "No internet connection"
+        sleep 2
+        clear
         exit 1
     fi
 }
@@ -30,16 +35,22 @@ cek_con() {
 cek_root () {
 	# Mengecek apakah pengguna saat ini adalah root atau bukan
 	if [[ $(whoami) != "root" ]]; then
- 	  echo -e "\nError: Anda harus menjalankan skrip ini sebagai root" 
-	  exit 1
+        clear
+ 	    echo -e "\nError: Anda harus menjalankan skrip ini sebagai root"
+        sleep 2
+        clear
+	    exit 1
 	fi
 }
 
 cek_distro () {
     # Mengecek apakah sistem operasi yang sedang berjalan merupakan basis Debian
     if ! cat /etc/*-release | grep -q "ID_LIKE=debian"; then
+        clear
         echo -e "\nSistem operasi ini bukan basis Debian."
         echo -e "\nScript ini hanya bekerja pada linux basis Debian ."
+        sleep 2
+        clear
         exit 1
     fi
 }
