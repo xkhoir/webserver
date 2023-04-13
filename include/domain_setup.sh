032a27f3fe337cffd70c6aa48a97a88a54c19e80
xkhoir <robby.khoirun1@gmail.com>
2023-04-13 08:48:28 +0700

    update logic apache conf setup fpm

--- include/domain_setup.sh
+++ include/domain_setup.sh
@@ -373,8 +373,7 @@ manage_vhost () {
     sleep 4
     
     # Tambah untuk php fpm
-    #isi="    <FilesMatch \\.php$>\n\tSetHandler \"proxy:unix:/run/php/$versi_terpilih.sock|fcgi://localhost/\"\n    </FilesMatch>"
-    sudo sed -i '12s/.*/'"    <FilesMatch \\.php$>\n\tSetHandler \"proxy:unix:/run/php/$versi_terpilih.sock|fcgi://localhost/\"\n    </FilesMatch>"'/' /etc/apache2/sites-available/$DOMAIN.conf
+    sudo sed -i '12s/.*/    <FilesMatch \\.php$>\n\tSetHandler "proxy:unix:\/run\/php\/'$versi_terpilih'.sock|fcgi:\/\/localhost\/"\n    <\/FilesMatch>/' tes.conf
     sleep 4
   fi
 }
