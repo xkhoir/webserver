4b40655d219cc18f5ea45c68c772c6a3c7b7cf58
Robby Khoirun <59682162+xkhoir@users.noreply.github.com>
2023-09-16 08:50:39 +0700

    change vhost setup

--- include/apache2.sh
+++ include/apache2.sh
@@ -3,12 +3,14 @@
 show_apache_submenu() {
     echo "1. Install"
     echo "2. Uninstall"
-    echo "3. Setup New Domain Directory"
-    echo "4. Setup SSL Let's Encrypt"
-    echo "5. Renew SSL Let's Encrypt"
-    echo "6. a2enmod rewrite"
-    echo "7. Restart/reload Apache Service"
-    echo "8. Kembali ke menu utama"
+    echo "3. Tambah Domain ke Vhost"
+    echo "4. Tambah Domain dengan Proxy ke Vhost"
+    echo "5. Hapus Domain dari Vhost"
+    echo "6. Setup SSL Let's Encrypt"
+    echo "7. Renew SSL Let's Encrypt"
+    echo "8. a2enmod rewrite"
+    echo "9. Restart/reload Apache Service"
+    echo "10. Kembali ke menu utama"
 }
 
 # fungsi untuk proses Apache
@@ -19,17 +21,20 @@ manage_apache() {
   if [ "$action" == "uninstall" ]; then
     #parsing data ke fungsi check_package
     check_package "$package" "$action"
-    rm -rf /var/lib/apache*
-    rm -rf /var/log/apache*
-    rm -rf /var/www/*
-    rm -rf /etc/apache*
-    rm -rf /etc/letsencry*
+    # rm -rf /var/lib/apache*
+    # rm -rf /var/log/apache*
+    # rm -rf /var/www/*
+    # rm -rf /etc/apache*
+    # rm -rf /etc/letsencry*
   elif [ "$action" == "install" ]; then
     #parsing data ke fungsi check_package
     check_package "$package" "$action"
-  elif [ "$action" == "domainsetup" ]; then
-    #call function domain setup
-    domain_setup "webserver"
+  elif [ "$action" == "adddomain" ]; then
+      domain_setup"$action"
+  elif [ "$action" == "addproxydomain" ]; then
+      domain_setup "$action"
+  elif [ "$action" == "deletedomain" ]; then
+      domain_setup "$action"
   elif [ "$action" == "ssl" ]; then
     #call function ssl setup
     ssl_setup "apache"
@@ -41,10 +46,11 @@ manage_apache() {
     a2enmod rewrite
   elif [ "$action" == "restart" ]; then
     #Proses restart/reload apache2 service
-    echo -e "Restart/reload service apache2\n"
+    echo -e "Restart/reload service $package\n"
     sleep 2
-    sudo systemctl reload apache2
-    sudo systemctl restart apache2
+    # sudo systemctl reload apache2
+    sudo systemctl reload $package
+    sudo systemctl restart $package
   else
     echo "Perintah tidak valid."
   fi
