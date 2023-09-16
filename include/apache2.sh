c954432dece862f486fbdcee2766b6532b3de418
Robby Khoirun <59682162+xkhoir@users.noreply.github.com>
2023-09-16 09:36:15 +0700

    Update apache2.sh

--- include/apache2.sh
+++ include/apache2.sh
@@ -30,7 +30,7 @@ manage_apache() {
     #parsing data ke fungsi check_package
     check_package "$package" "$action"
   elif [ "$action" == "adddomain" ]; then
-      domain_setup"$action"
+      domain_setup "$action"
   elif [ "$action" == "addproxydomain" ]; then
       domain_setup "$action"
   elif [ "$action" == "deletedomain" ]; then
